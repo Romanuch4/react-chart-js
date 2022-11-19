a2340a14860a768da1ce4fcc1998be95e6023d5a
Romanuch4 <pomankrentovsky@gmail.com>
2022-11-19 16:57:16 +0200

    test

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -12,7 +12,7 @@ const TestPage = () => {
   const data = useMemo(
     () => ({
       xLabels: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-      yLabels: [0, 10, 20, 30, 40, 50],
+      yLabels: [0, 10, 20, 30, 40, 50, 60],
     }),
     [],
   )
