9dd1ef67595a78ca1fdd1f2206d7de343c60d36a
Romanuch4 <pomankrentovsky@gmail.com>
2022-11-20 15:16:24 +0200

    [T-1] Started to do a chart

--- src/pages/test/Test.test.tsx
+++ src/pages/test/Test.test.tsx
@@ -1,9 +1,9 @@
-import { screen, render } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import TestPage from '.'
 
 describe('App', () => {
   test('Should render correctly', () => {
     render(<TestPage />)
-    expect(screen.getByText('Test Page')).toBeInTheDocument()
+    expect(document.querySelector('canvas')).toBeInTheDocument()
   })
 })
